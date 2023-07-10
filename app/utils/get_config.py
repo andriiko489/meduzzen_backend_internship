2b47955b3f6afa1337c2222c0266334fa27a6765
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-10 22:46:29 +0300

    in get_config change default path to env

--- app/utils/get_config.py
+++ app/utils/get_config.py
@@ -2,8 +2,8 @@ from dotenv import dotenv_values
 from pathlib import Path
 
 def get_config():
-    root = Path().absolute().parent
+    root = Path().absolute()
     config_env = {
-        **dotenv_values(str(root) + "\.env"),
+        **dotenv_values(str(root) + "\.sample.env"),
     }
     return config_env
