870baaa538d0559fe1ff9e60aeac9a6aa20c8c96
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-10 22:52:10 +0300

    add if name condition. add host and port to env

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 
 from app.utils.get_config import get_config
 
 config_env = get_config()
 app = FastAPI()
+print(config_env)
 
 
 @app.get("/")
@@ -12,3 +14,7 @@ def health_check():
             "detail": "ok",
             "result": "working"
             }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=config_env["HOST"], port=int(config_env["PORT"]))
