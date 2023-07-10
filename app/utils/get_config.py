98418e11b4027fd3f884a7d19294cbf52bff0e91
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-11 00:03:44 +0300

    edit env sample

--- app/utils/get_config.py
+++ app/utils/get_config.py
@@ -4,6 +4,6 @@ from pathlib import Path
 def get_config():
     root = Path().absolute()
     config_env = {
-        **dotenv_values(str(root) + "\.sample.env"),
+        **dotenv_values(str(root) + "\.env.sample"),
     }
     return config_env
