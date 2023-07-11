b81c3fe7b3ca44284bc7943caf854f37f3b749dd
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-11 15:32:56 +0300

    attempt to do CORS Middleware

--- app/main.py
+++ app/main.py
@@ -2,9 +2,24 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.utils.config import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def health_check():
