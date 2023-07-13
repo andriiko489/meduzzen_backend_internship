f4e434c683b66a4bfc7aeb344904a6550bfc8270
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-13 20:35:58 +0300

    split main to files

--- app/main.py
+++ app/main.py
@@ -2,50 +2,10 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-import sqlalchemy
-
-from pydantic import BaseModel
 from utils.config import settings
 
-import redis.asyncio as redis
-import asyncio
-import nest_asyncio
-
-nest_asyncio.apply()
-
-
-async def connect_redis():
-    await redis.from_url("redis://redis:6379")
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(connect_redis())
-# SQLAlchemy specific code, as with any other app
-
-metadata = sqlalchemy.MetaData()
-
-notes = sqlalchemy.Table(
-    "notes",
-    metadata,
-    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("text", sqlalchemy.String),
-    sqlalchemy.Column("completed", sqlalchemy.Boolean),
-)
-
-engine = sqlalchemy.create_engine(
-    settings.database_url
-)
-metadata.create_all(engine)
-
-
-class NoteIn(BaseModel):
-    text: str
-    completed: bool
-
-
-class Note(NoteIn):
-    id: int
-
+from db.connect_to_redis import r as redis
+import db.connect_to_pgdb
 
 app = FastAPI()
 
