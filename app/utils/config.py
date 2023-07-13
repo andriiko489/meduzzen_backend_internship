a099f11c1eeca7163426e2c3cf9e919ffe36da11
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-13 14:53:02 +0300

    add postfix

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,13 +1,14 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
-path = Path(str(Path().absolute()) + "/.env")  # This peace of code needed for run configurations and Current file in
-if not path.is_file():                         # Pycharm
-    path = Path(str(path.parent.parent) + "/.env")
-
+postfix = "/.env"
+path = Path(str(Path().absolute()) + postfix)  # This peace of code needed for run configurations and Current file in
+while not path.is_file():                         # Pycharm
+    path = Path(str(path.parent.parent) + postfix)
+print(path)
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=str(path))
+    model_config = SettingsConfigDict(env_file=path)
     host: str
     port: int
 
