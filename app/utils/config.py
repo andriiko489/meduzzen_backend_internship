7dc73043e11f094ba2c40428cb2e197d457f8335
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-14 13:10:51 +0300

    remove printing path to env

--- app/utils/config.py
+++ app/utils/config.py
@@ -5,7 +5,6 @@ found_dotenv = dotenv.find_dotenv('.env')
 
 
 class Settings(BaseSettings):
-    print(found_dotenv)
     model_config = SettingsConfigDict(env_file=found_dotenv)
     host: str
     port: int
