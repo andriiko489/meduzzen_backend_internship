92d59874e68d3ffeb3313b483a431175f0fcceb9
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-17 16:48:47 +0300

    delete importing delete function from sqlalchemy

--- app/db/crud.py
+++ app/db/crud.py
@@ -1,5 +1,5 @@
 from fastapi import HTTPException
-from sqlalchemy import select, delete
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from schemas import models, schemas
