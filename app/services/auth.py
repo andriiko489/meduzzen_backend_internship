a12b650b68a352d2aec91f6fd8521f8480d87231
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-19 22:47:29 +0300

    refactor auth and hasher.py

--- app/services/auth.py
+++ app/services/auth.py
@@ -1,13 +1,64 @@
-from passlib.context import CryptContext
+from __future__ import annotations
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from datetime import timedelta, datetime
+import jwt
+
+from crud.UserCRUD import user_crud
+from services.hasher import Hasher
+from utils.config import settings
 
 
-class Hasher:
-    @staticmethod
-    def verify_password(plain_password, hashed_password):
-        return pwd_context.verify(plain_password, hashed_password)
+class Auth:
+    async def authenticate_user(username: str, password: str):
+        user = await user_crud.get_by_username(username)
+        if not user:
+            return False
+        if not Hasher.verify_password(password, user.hashed_password):
+            return False
+        return user
 
-    @staticmethod
-    def get_password_hash(password):
-        return pwd_context.hash(password)
+    def create_access_token(data: dict, expires_delta: timedelta | None = None):
+        to_encode = data.copy()
+        if expires_delta:
+            expire = datetime.utcnow() + expires_delta
+        else:
+            expire = datetime.utcnow() + timedelta(minutes=15)
+        to_encode.update({"exp": expire})
+        encoded_jwt = jwt.encode(to_encode, settings.secret_key, algorithm=settings.algorithm)
+        return encoded_jwt
+
+
+class VerifyToken:
+    """Does all the token verification using PyJWT"""
+
+    def __init__(self, token):
+        self.token = token
+
+        # This gets the JWKS from a given URL and does processing so you can
+        # use any of the keys available
+        jwks_url = f'https://{settings.domain}/.well-known/jwks.json'
+        self.jwks_client = jwt.PyJWKClient(jwks_url)
+
+    def verify(self):
+        # This gets the 'kid' from the passed token
+        try:
+            self.signing_key = self.jwks_client.get_signing_key_from_jwt(
+                self.token
+            ).key
+        except jwt.exceptions.PyJWKClientError as error:
+            return {"status": "error", "msg": error.__str__()}
+        except jwt.exceptions.DecodeError as error:
+            return {"status": "error", "msg": error.__str__()}
+
+        try:
+            payload = jwt.decode(
+                self.token,
+                self.signing_key,
+                algorithms=settings.algorithms,
+                audience=settings.api_audience,
+                issuer=settings.issuer,
+            )
+        except Exception as e:
+            return {"status": "error", "message": str(e)}
+
+        return payload
