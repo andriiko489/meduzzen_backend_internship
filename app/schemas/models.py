e2b894cf433086ced19c88b139ab89f231523e38
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-25 14:03:18 +0300

    added Company model

--- app/schemas/models.py
+++ app/schemas/models.py
@@ -1,5 +1,7 @@
+from typing import List
+
 from sqlalchemy import Boolean, Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, relationship
 
 
 class Base(DeclarativeBase):
@@ -14,3 +16,12 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
+    company: Mapped[List["Company"]] = relationship(back_populates="user")
+
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    owner: Mapped["User"] = relationship(back_populates="company")
