5d08fef80044eaa24d3a638ae4bab4f6b042a9e9
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-26 00:38:42 +0300

    edit name of file

--- app/tests/connect_to_testdb.py
+++ app/tests/connect_to_testdb.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from crud.UserCRUD import UserCRUD
 from utils.config import settings
 
+
 test_engine = create_async_engine(settings.test_database_url, echo=True)
 test_async_session = sessionmaker(
     test_engine, class_=AsyncSession, expire_on_commit=False
