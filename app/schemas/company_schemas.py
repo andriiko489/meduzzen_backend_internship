606918a17ec331b184cd43bdd8541f3ae286220f
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-25 15:05:46 +0300

    rename company schemas to company_schemas.py

--- app/schemas/company_schemas.py
+++ app/schemas/company_schemas.py
@@ -7,3 +7,8 @@ class Company(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: Optional[int] = None
     name: str
+
+
+class AddCompany(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    name: str
