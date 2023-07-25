4872603c706f7e71723a55ec78cd6f0a03b17a62
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-07-26 00:41:52 +0300

    update company in models.py

--- app/schemas/models.py
+++ app/schemas/models.py
@@ -8,14 +8,6 @@ class Base(DeclarativeBase):
     pass
 
 
-class Company(Base):
-    __tablename__ = "companies"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    owner: Mapped["User"] = relationship(back_populates="company", lazy="selectin")
-
-
 class User(Base):
     __tablename__ = "users"
 
@@ -24,5 +16,13 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
-    company_id: Mapped[Optional[int]] = mapped_column(ForeignKey("companies.id"))
-    company: Mapped[List["Company"]] = relationship(back_populates="owner", lazy="selectin")
+    company: Mapped[List["Company"]] = relationship(back_populates="owner")
+
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    owner_id: Mapped[Optional[int]] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped["User"] = relationship(back_populates="company", lazy="subquery")
