cad471e4e55ef0e1ee1fa3e126059bf2036e08ae
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-08-02 00:32:28 +0300

    rename test-db-connections-file and add admin testing

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -7,7 +7,7 @@ from fastapi.testclient import TestClient
 from main import app
 from schemas import user_schemas, basic_schemas, invitation_schemas
 
-from tests.connect_to_testdb import user_crud_test, company_crud_test, test_session, invitation_crud_test
+from tests.testdb import user_crud_test, company_crud_test, test_session, invitation_crud_test, admin_crud_test
 from sqlalchemy.sql import text as sa_text
 
 client = TestClient(app)
@@ -34,15 +34,15 @@ async def test_crud():
     db_user = await user_crud_test.add(user)
     assert user is not None
     assert db_user.username == test_string
-    db_user_id = db_user.id
+    user_id = db_user.id
 
     assert user.username == test_string
     company = basic_schemas.Company(name="PEPSICO", owner_id=db_user.id)
     db_company = await company_crud_test.add(company)
     assert db_company is not None
     assert await company_crud_test.get_company(db_company.id)
-    db_company_id = db_company.id
-    assert await user_crud_test.set_company(db_company_id, db_user_id)
+    company_id = db_company.id
+    assert await user_crud_test.set_company(company_id, user_id)
 
     test_string_2 = get_random_string(length=8)
     user_2 = user_schemas.User(username=test_string_2, hashed_password=test_string_2, email=test_string_2,
@@ -50,12 +50,16 @@ async def test_crud():
     db_user_2 = await user_crud_test.add(user_2)
     assert user_2 is not None
     assert db_user_2.username == test_string_2
-    db_user_2_id = db_user_2.id
+    user_2_id = db_user_2.id
 
-    invitation = invitation_schemas.BasicInvitation(sender_id=db_user_id, receiver_id=db_user_2_id, company_id=db_company_id)
+    invitation = invitation_schemas.BasicInvitation(sender_id=user_id, receiver_id=user_2_id, company_id=company_id)
     db_invitation = await invitation_crud_test.add(invitation)
     assert db_invitation is not None
     assert await invitation_crud_test.get(db_invitation.id)
-    db_invitation_id = db_invitation.id
-    assert await invitation_crud_test.accept_invitation(db_invitation_id)
-    assert (await user_crud_test.get_user(db_user_2_id)).company_id == db_company_id
+    invitation_id = db_invitation.id
+    assert await invitation_crud_test.accept_invitation(invitation_id)
+    assert (await user_crud_test.get_user(user_2_id)).company_id == company_id
+
+    admin = basic_schemas.BasicAdmin(company_id=company_id, user_id=user_2_id)
+    db_admin = await admin_crud_test.set_admin(admin)
+    assert db_admin is not None
