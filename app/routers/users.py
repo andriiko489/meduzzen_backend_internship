ef75d8cb9fab85e9c306dcac41916c980eb534d5
andriiko489 <51889954+andriiko489@users.noreply.github.com>
2023-08-07 11:39:13 +0300

    simplify get rate

--- app/routers/users.py
+++ app/routers/users.py
@@ -93,9 +93,6 @@ async def get_me(current_user: user_schemas.User = Depends(Auth.get_current_user
 @router.get("/rate")
 async def get_rate(current_user: user_schemas.User = Depends(Auth.get_current_user)):
     finished_quizzes = await finished_quiz_crud.get_by_user_id(current_user.id)
-    total_num_of_questions = 0
-    total_scores = 0
-    for quiz in finished_quizzes:
-        total_num_of_questions += quiz.num_of_questions
-        total_scores += quiz.num_of_correct_answers
-    return total_scores/total_num_of_questions
+    total_num_of_questions = sum(quiz.num_of_questions for quiz in finished_quizzes)
+    total_scores = sum(quiz.num_of_correct_answers for quiz in finished_quizzes)
+    return total_scores / total_num_of_questions if total_num_of_questions > 0 else 0
